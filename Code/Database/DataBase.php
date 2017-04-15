b3f9ed1be4790af8d70e81a8c9802f100068b19a
خالد هشام <khaledhesham2017@hotmail.com>
2017-04-15 16:43:17 +0200

    edit class DataBase  to support arabic

--- Code/Database/DataBase.php
+++ Code/Database/DataBase.php
@@ -14,41 +14,28 @@ class DataBase
   private $password;
   private $database;
   private $connection;
-  private $file_name= 'credential.php';
-  function __construct()
+  function __construct($file_name)
   {
 
     # code...
     try {
-    include_once $this->file_name ;
+    include_once $file_name ;
       } catch (Exception $e) {
       echo "error in file name";
     }
-
     $this->host=$SERVER;
     $this->user=$USERNAME;
     $this->password=$PASSWORD;
     $this->database=$DATABASE;
     $this->connection();
-
-
-  }
+}
   private  function connection(){
     //connect to server
-
-  if(! ($this ->con =mysqli_connect($this->host,$this->user,$this->password,$this->database)))
-  {
-
-    throw new Exception("Error: not connected to server");
-    }
-    if (mysqli_connect_errno())
-   {
-   echo "Failed to connect to MySQL: " . mysqli_connect_error();
+   $this ->con =mysqli_connect($this->host,$this->user,$this->password,$this->database)
+   or die ( mysqli_error($MySQL_Handle) );
    }
-
-
-  }
    function close_connection(){
+     //close connection
     mysqli_close($this->con);
    }
    function get_con(){
@@ -56,22 +43,25 @@ class DataBase
    }
    public function fetch_query($query)
    {
+     // applay query and return assc aaray
      # code...
      return $this->database_all_assoc($this->database_query($query));
    }
    public function database_query($database_query) {
-
+     //aplay query
+     $sSQL= 'SET CHARACTER SET utf8';
+      mysqli_query($this->con,$sSQL) ;
        $query_result = mysqli_query($this->con,$database_query);
        return $query_result;
    }
    public function database_all_assoc($database_result) {
-
+      // return assc array
         while ($row = mysqli_fetch_assoc($database_result)) {
             $array_return[] = $row;
         }
 
         return $array_return;
-    }
+  }
 
   }
 
--- Code/Database/order.php
+++ Code/Database/order.php
@@ -6,6 +6,7 @@ class Order_Query
 {
   private $file_name ='DataBase.php';
   private $database;
+  private $file_name2= 'credential.php';
   function __construct()
   {
     try {
@@ -14,17 +15,15 @@ class Order_Query
      echo "error in file name";
    }
 
-     $this->database = new DataBase();
-     $this->fech_word(1);
-
-  }
-   public function fech_word($id=1)
+     $this->database = new DataBase($this->file_name2);
+}
+   public function fetch_word($id)
   {
     # code...
 
     $query = "SELECT `word_id`, `en_word`, `ar_word` FROM `app_language` WHERE word_id =$id";
-    $reselt =$this->database->fetch_query($query);
-    var_dump ($reselt);
+     return ($this->database->fetch_query($query));
+
   }
 
 
