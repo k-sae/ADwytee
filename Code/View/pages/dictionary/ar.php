6467ddfb153379babda872a143888e535607f4b5
Belal <billy.fcih@yahoo.com>
2017-04-19 14:42:13 +0200

    Add language selection

--- Code/View/pages/dictionary/ar.php
+++ Code/View/pages/dictionary/ar.php
@@ -1,8 +1,14 @@
 //this  file  ar word
 <?php
 
+$language['logo'] = "أدويتي";
+$language['search'] = "أبحث";
 $language['orders'] = "الطلبات";
 $language['page'] ="صفحة";
 $language['orderpage'] ="صفحة الطلبات";
+$language['pharmacyprofile'] ="صفحة الصيديلة الشخصية";
+$language['about'] ="عنّا";
+$language['register'] ="سجّل";
+$language['contact'] ="تواصل معنا";
 
- ?>
+?>
--- Code/View/pages/dictionary/en.php
+++ Code/View/pages/dictionary/en.php
@@ -1,7 +1,14 @@
 //this  file  en word
 <?php
 
+$language['logo'] = "ADwytee";
+$language['search'] = "Search";
 $language['orders'] = "Orders";
 $language['page'] ="Page";
 $language['orderpage'] ="OrderPage";
- ?>
+$language['pharmacyprofile'] ="Pharmacy Profile";
+$language['about'] ="About us";
+$language['register'] ="Register";
+$language['contact'] ="Contact Us";
+
+?>
