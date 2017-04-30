f8bf308bc15888f2dbfa6eb5b731ee0d116ef52f
خالد هشام <khaledhesham2017@hotmail.com>
2017-04-30 14:29:40 +0200

    fix database

--- Code/Database/order.php
+++ Code/Database/order.php
@@ -20,25 +20,25 @@ class Order_Query
    public function fetch_order($id)
   {
     # code...
-    $query = "SELECT `Id`, `PharmacyId`, `date`, `status` FROM `Order` WHERE `UserId` = $id ";
+    $query = "SELECT `Id`, `PharmacyId`, `date`, `status` FROM `ORDER` WHERE `UserId` = $id ";
     return ($this->database->fetch_query($query));
     }
     public function fetch_pharmacy($id){
-        $query = "SELECT `Name` FROM `Pharmacy` WHERE `UserId` = $id ";
+        $query = "SELECT `Name` FROM `PHARMACY` WHERE `UserId` = $id ";
         return ($this->database->fetch_query($query));
     }
     public function fetch_all_pharmacy(){
-        $query = "SELECT `Name` FROM `Pharmacy`";
+        $query = "SELECT `Name` FROM `PHARMACY`";
         return ($this->database->fetch_query($query));
     }
     public function deleteOrder($id)
     {
-      $query ="DELETE FROM `Order` WHERE `id`=$id";
+      $query ="DELETE FROM `ORDER` WHERE `id`=$id";
       $this->database->fetch_query($query);
     }
   public function add()
   {
-    $query ="INSERT INTO `Usertype`( `Type`) VALUES ('m5ns')";
+    $query ="INSERT INTO `USERTYPE`( `Type`) VALUES ('m5ns')";
     $this->database->database_query($query);
   }
 }
