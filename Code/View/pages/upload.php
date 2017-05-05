7fe06ae8ca973f00cd02494d73000ab753b633a8
kareem2048 <karim.salah2048@gmail.com>
2017-05-06 00:35:09 +0200

    finish basic upload functions

---
+++ Code/View/pages/upload.php
@@ -0,0 +1,20 @@
+<?php
+$target_dir = "../mimages/";
+$target_file = $target_dir . basename($_POST["m_code"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+	$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+	if($check !== false) {
+		echo "File is an image - " . $check["mime"] . ".";
+		$uploadOk = 1;
+	} else {
+		echo "File is not an image.";
+		$uploadOk = 0;
+	}
+	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+		echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+}
+}
+?>
