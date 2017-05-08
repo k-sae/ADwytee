2028bf1c356fc8091f03c0c18c45a96fc448c9c6
Belal <billy.fcih@yahoo.com>
2017-05-08 09:41:06 +0200

    Add orderpage to pharmacy navbar

--- Code/View/pages/nav/nav2.php
+++ Code/View/pages/nav/nav2.php
@@ -1,5 +1,6 @@
 <?php include_once 'language.php'; ?>
 
 <li ><a href="Pharmacy.php"><?php echo  $language['pharmacyprofile']?></a></li>
+<li class="<?php if($page=="orderPage"){echo "active";}?>"><a href="orderPage.php"><?php echo  $language['orderpage']?></a></li>
 <li class="<?php if($page=="pharmacyorder"){echo "active";}?>"><a href="pharmacyOrder.php"><?php echo  $language['pharmacyorder']?></a></li>
 <li><a href="logout.php"><?php echo  $language['logout']?></a></li>
