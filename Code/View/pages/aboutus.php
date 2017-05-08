a37178a97d52a5ce7066d8bf444693a12e5bb555
mostafa7azem <mostafahazem144@yahoo.com>
2017-05-08 08:51:49 +0200

    about us added

---
+++ Code/View/pages/aboutus.php
@@ -0,0 +1,45 @@
+<?php
+include '../content/header.php';?>
+<div class="aboutus">
+    <div class="container" style="margin-top:50px">
+        <div class="about row center">
+    <div class="col-xs-12">
+    <h1 class="text-center">About us</h1>
+    <p class="lead">We prefer working with each client individually.
+With an average of more than 31 million visits each month, Cars is the leading online destination for car shoppers and owners. We offer credible and easy-to-understand information to help consumers research, price and find new and used vehicles and quality service and repair providers.</p>
+		</div>
+    	</div>
+    </div>
+</div>
+<div class="aboutus1">
+	<div class="container">
+    	<h2 class="text-center">Meet our team</h2>
+        <div class="row">
+            <div class="teamimg col-xs-6 col-md-3">
+            <img src="images/mt-0310-about-img05.jpg">
+            <h3>Alan Smith</h3>
+            <p>Vice President of Product Department</p>
+            </div>
+             <div class="teamimg col-xs-6 col-md-3">
+            <img src="images/mt-0310-about-img04.jpg">
+            <h3>John McCoist</h3>
+            <p>Chief Executive Officer and President</p>
+            </div>
+             <div class="teamimg col-xs-6 col-md-3">
+            <img src="images/mt-0310-about-img03.jpg">
+            <h3>Bradley Grosh</h3>
+<p>Chief Marketing Officer</p>
+            </div>
+             <div class="teamimg col-xs-6 col-md-3">
+            <img src="images/mt-0310-about-img02.jpg">
+            <h3>Laura Stegner</h3>
+<p>Vice President of Sales Department</p>
+            </div>
+        </div>
+    </div>
+<div>
+</div>
+</div>
+<?php
+include '../content/footer.php';
+?>
